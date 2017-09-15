a73220b9564b864267479ac016941fe917ea77a3
Akash Kumar <akash@users.noreply.github.com>
2017-09-15 13:32:35 -0400

    Animation & Modal polish
    the text was white..

--- functions.js
+++ functions.js
@@ -10,3 +10,14 @@ bodymovin.loadAnimation({
 $("#element").click(function() {
   window.location = "http://akash.nyc/Resume.pdf";
 });
+
+$(window).load(function () {
+    //normally you'd wait for document.ready, but you'd likely to want to wait
+    //for images to load in case they reflow the page
+    $('#element').delay(5000) //wait 5 seconds
+        .animate({
+            //animate jQuery's custom "scrollTop" style
+            //grab the value as the offset of #second from the top of the page
+            'scrollTop': $('#content').offset().top
+        }, 300); //animate over 300ms, change this to however long you want it to animate for
+});
