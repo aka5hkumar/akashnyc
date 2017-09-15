1c6661cc281354b88cb5ae8d08ec65756a61ee1f
Akash Kumar <akash@users.noreply.github.com>
2017-09-15 00:52:25 -0400

    Updated with new design
    Finally fixed the animation issues

---
+++ functions.js
@@ -0,0 +1,11 @@
+bodymovin.loadAnimation({
+  container: element, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: "logo.json", // the path to the animation json
+});
+
+$("#element").click(function() {
+    window.location = "http://akash.nyc/Resume.pdf";
+});
