d9b3b72056ce7ea7700c65d7feb6e72c362654b8
Akash Kumar <akash@users.noreply.github.com>
2017-09-15 14:54:47 -0400

    Ready for content
    Nixed autoscrolling

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-AOS.init();
 bodymovin.loadAnimation({
   container: element, // the dom element that will contain the animation
   renderer: 'svg',
@@ -11,13 +10,6 @@ $("#element").click(function() {
   window.location = "http://akash.nyc/Resume.pdf";
 });
 
-$(window).load(function () {
-    //normally you'd wait for document.ready, but you'd likely to want to wait
-    //for images to load in case they reflow the page
-    $('#element').delay(5000) //wait 5 seconds
-        .animate({
-            //animate jQuery's custom "scrollTop" style
-            //grab the value as the offset of #second from the top of the page
-            'scrollTop': $('#content').offset().top
-        }, 300); //animate over 300ms, change this to however long you want it to animate for
-});
+// $('.container').delay(3000).animate({
+//     scrollTop: $('#content').offset().top
+//   }, 2000);
