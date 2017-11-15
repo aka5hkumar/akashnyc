1feb2ad2d82be32cf04900a81cd534b27dd314d4
Akash Kumar <aka5hkumar@users.noreply.github.com>
2017-11-15 11:04:59 -0500

    I have a portfolio now....

--- portfolio/functions.js
+++ portfolio/functions.js
@@ -1,15 +1,11 @@
 bodymovin.loadAnimation({
   container: element, // the dom element that will contain the animation
   renderer: 'svg',
-  loop: true,
+  loop: 1,
   autoplay: true,
   path: "logo.json", // the path to the animation json
 });
 
 $("#element").click(function() {
-  window.location = "http://akash.nyc/Resume.pdf";
+  window.location = "http://akash.nyc/#content";
 });
-
-// $('.container').delay(3000).animate({
-//     scrollTop: $('#content').offset().top
-//   }, 2000);
