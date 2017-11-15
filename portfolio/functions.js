4f335c990f26fa7f23db863a167000911f1d3f0f
Akash Kumar <aka5hkumar@users.noreply.github.com>
2017-11-15 16:24:20 -0500

    button

--- portfolio/functions.js
+++ portfolio/functions.js
@@ -7,5 +7,24 @@ bodymovin.loadAnimation({
 });
 
 $("#element").click(function() {
-  window.location = "http://akash.nyc/#content";
+  window.location = "../#content";
+});
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        $('#back-to-top').tooltip('show');
+
 });
